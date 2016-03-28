c75fa34d3b0711a7f2f4035c4695d7f317ad1944
Bill Cho <tw.billcho.dev@gmail.com>
2016-03-28 10:51:35 +0800

    Add find contours strategy.

--- strategy.py
+++ strategy.py
@@ -11,7 +11,7 @@ class alternativeStrategy(object):
         print("Please implement an execution method." )
 
 # Define each image process strategy
-# Define image difference 
+# Define image difference strategy
 class diffStrategy(alternativeStrategy):
     def __init__(self):
         pass
@@ -22,10 +22,10 @@ class diffStrategy(alternativeStrategy):
         [image1, image2] = [images[0], images[-1]]
         # Calculate difference
         self.diffImage = cv2.absdiff (image1, image2)
-        #return self.diffImage
+        # save the processed image
         imageContainer.insert({"Process": self.diffImage})
 
-# Define image blur 
+# Define image blur strategy
 class blurStrategy(alternativeStrategy):
     def __init__(self):
         self.blurSetting=(3,3)
@@ -34,10 +34,10 @@ class blurStrategy(alternativeStrategy):
         image = imageContainer.pop("Process")
         # Blur the image
         self.blurImage = cv2.blur(image, self.blurSetting)
-        #return self.blurImage
+        # save the processed image
         imageContainer.insert({"Process": self.blurImage})
 
-# Define image threshod 
+# Define image threshod strategy
 class thresStrategy(alternativeStrategy):
     def __init__(self):
         self.thresValue = 32
@@ -47,8 +47,31 @@ class thresStrategy(alternativeStrategy):
         image = imageContainer.pop("Process")
         # Apply threshold (enhanced)
         ret, self.thresImage = cv2.threshold(image, self.thresValue, self.thresMaxVal, cv2.THRESH_BINARY)
-        #return self.thresImage
+        # save the processed image
         imageContainer.insert({"Process": self.thresImage})
+
+# Define find contours strategy
+class findContoursStrategy(alternativeStrategy):
+    def __init__(self):
+        pass
+
+    def execute(self, imageContainer):
+        image = imageContainer.pop("Process")
+        # convert to gray scale 
+        gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        # Find contours
+        max_contours=0
+        contours, hierarchy = cv2.findContours(gray_image, cv2.RETR_LIST, cv2.CHAIN_APPROX_TC89_KCOS)
+        for contour in contours:
+            x,y,w,h = cv2.boundingRect(contour)
+            # Filter out the small boundaries
+            if w*h > 225:
+                cv2.rectangle(image, (x,y), (x+w,y+h), (0,255,0), 2)
+                # Record the max contour
+                if w*h > max_contours:
+                    max_contours = w*h
+        # save the processed image
+        imageContainer.insert({"Process": image})
 
 # Constuctor of these strategies above
 class strategyConstructor():
@@ -58,6 +81,7 @@ class strategyConstructor():
         self.strategyList.append ( diffStrategy() )
         self.strategyList.append ( blurStrategy() )
         self.strategyList.append ( thresStrategy() )
+        self.strategyList.append ( findContoursStrategy() )
 
     def listStrategy(self):
         return self.strategyList
