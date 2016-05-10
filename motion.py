939aa52375637e84d89c4b8d780b3cbe3594f746
Bill Cho <tw.billcho.dev@gmail.com>
2016-05-11 00:01:55 +0800

    Add X11 GUI checking.

--- motion.py
+++ motion.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import cv2
+
 class motion():
     def __init__(self):
         # Construct the camera capture object
@@ -14,38 +16,61 @@ class motion():
         # Construct the image processing strategy
         from strategy import strategyConstructor
         self.strategyConstruction = strategyConstructor(self.eventAlarm)
+        # check if X11 DISPLAY exist
+        self._isgui = self._checkGUI()
 
     def detect(self):
-        import cv2
         # Store initial n images to image container
         for i in range(3):
             # Capture a image
-            ret, image = self.cam.read()
-            self.imgContainer.insert ({"Original": image })
+            self.fetchImage()
 
         while True:
             # reset alarm
             self.eventAlarm.reset()
-
             # Runing the image process strategies
             for strategy in self.strategyConstruction.listStrategy():
                 strategy.execute(self.imgContainer)
-
-            # Display the image results:
-            cv2.imshow ("Original Image", self.imgContainer.pop("Original") )
-            cv2.imshow ("Image process", self.imgContainer.pop("Process") )
-            cv2.waitKey(20)
-
+            # Display image
+            self.showImage()
             # Check alarm
             if self.eventAlarm.isalarm():
                 self.eventAlarm.alarm()
-
             # Capture new image
-            ret, image = self.cam.read()
-            self.imgContainer.insert ({"Original": image })
+            self.fetchImage()
+        # Close window and exit the program
+        self.closeWindow()
 
-        # Exist the program
-        cv2.destroyAllWindows()
+    def fetchImage(self):
+        # Capture a image
+        ret, image = self.cam.read()
+        self.imgContainer.insert ({"Original": image })
+
+    def showImage(self):
+        # Display the image results:
+        if (self._isGUI()):
+            cv2.imshow ("Original Image", self.imgContainer.pop("Original") )
+            cv2.imshow ("Image process", self.imgContainer.pop("Process") )
+            cv2.waitKey(20)
+        else:
+            self.imgContainer.pop("Original")
+            self.imgContainer.pop("Process")
+
+    def closeWindow(self):
+        # Close all cv2 window
+        if (self._isGUI()):
+            cv2.destroyAllWindows()
+        else:
+            exit()
+
+    def _checkGUI(self):
+        # check if X11 DISPLAY exist
+        import os
+        return os.environ.has_key('DISPLAY')
+
+    def _isGUI(self):
+        # check X11 DISPLAY exist
+        return self._isgui
 
 # Run the program
 if __name__ == "__main__":
